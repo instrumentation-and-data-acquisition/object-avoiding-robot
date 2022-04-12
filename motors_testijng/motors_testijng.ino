b9b7c244ffc9706df3597b4cfc6ee1d4b6c90717
ana <anaisabelmartins@tecnico.ulisboa.pt>
2022-04-12 20:01:28 +0100

    robot now walking straight slowly

--- motors_testijng/motors_testijng.ino
+++ motors_testijng/motors_testijng.ino
@@ -1,21 +1,34 @@
-int motor1 = A0;
-int motor2 = A1;
-
-int i = 0;
-
-
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(motor1, OUTPUT);
-  pinMode(motor2, OUTPUT);
+//Motor 1
+const int motorPin1 = 9;
+const int motorPin2 = 8;
+//Motor 2
+const int motorPin3 = 7;
+const int motorPin4 = 6;
+int speed = 255;
+void setup(){
+//Set pins as outputs
+pinMode(motorPin1, OUTPUT);
+pinMode(motorPin2, OUTPUT);
+pinMode(motorPin3, OUTPUT);
+pinMode(motorPin4, OUTPUT);
+//Motor Control A in both directions
 
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-    analogWrite(motor1, 255);
-    analogWrite(motor2, 255);
-
-
+void loop(){
+  analogWrite(motorPin1, speed);
+  delay(25);
+  analogWrite(motorPin1, 0);
+  delay(1);
+  analogWrite(motorPin2, speed);
+  delay(25);
+  analogWrite(motorPin2, 0);
+  analogWrite(motorPin3, speed);
+  delay(25);
+  analogWrite(motorPin3, 0);
+  delay(1);
+  analogWrite(motorPin4, speed);
+  delay(25);
+  analogWrite(motorPin4, 0);
+  
+  
 }
