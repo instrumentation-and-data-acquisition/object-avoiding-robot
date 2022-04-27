ada257a647c5df01f0c2793868dc9c6649aa62da
ana <anaisabelmartins@tecnico.ulisboa.pt>
2022-04-27 22:04:09 +0100

    merging with last one

--- motor_sensor_testing/motor_sensor_testing.ino
+++ motor_sensor_testing/motor_sensor_testing.ino
@@ -1,76 +1,79 @@
 
-//Motor 1
-const int motorPin1 = 9;     //amarelo
-const int motorPin2 = 8;    //amarelo
-//Motor 2
-const int motorPin3 = 7;    //vermelho
-const int motorPin4 = 6;   //branco
+// Motor 1
+const int motorPin1 = 9; // amarelo
+const int motorPin2 = 8; // amarelo
+// Motor 2
+const int motorPin3 = 7; // vermelho
+const int motorPin4 = 6; // branco
 int speed = 255;
 
-const int PhotoIn = 2; 
-int State=0 ; 
+const int PhotoIn = 2;
+int State = 0;
 
 // SENSOR DE DISTâNCIA FRENTE
+int sensorFrente = A0;
+int sensorTras = A1;
 int sensorFrente = A1;
 int sensorTras = A0;
-int sensorArd= A2; 
-int sensorAmp = A5; 
+int sensorArd = A2;
+int sensorAmp = A5;
+
+int maxDistFrente = 650;
+int maxDistTras = 850;
 
 int valueFrente;
 int valueTras;
-int valueArd; 
+int valueArd;
 int valueAmp;
 
-void setup(){
-Serial.begin(9600);
-//Set pins as outputs
-pinMode(motorPin1, OUTPUT);
-pinMode(motorPin2, OUTPUT);
-pinMode(motorPin3, OUTPUT);
-pinMode(motorPin4, OUTPUT);
-pinMode(PhotoIn, INPUT); 
-pinMode(sensorFrente, INPUT);
-pinMode(sensorTras, INPUT);
-pinMode(sensorArd, INPUT);
-pinMode(sensorAmp, INPUT);
+void setup()
+{
+  Serial.begin(9600);
+  // Set pins as outputs
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  pinMode(motorPin3, OUTPUT);
+  pinMode(motorPin4, OUTPUT);
+  pinMode(PhotoIn, INPUT);
+  pinMode(sensorFrente, INPUT);
+  pinMode(sensorTras, INPUT);
+  pinMode(sensorArd, INPUT);
+  pinMode(sensorAmp, INPUT);
 
-//Motor Control A in both directions
+  // Motor Control A in both directions
 
-  do {
-  analogWrite(motorPin1, speed);
-  delay(120);
-  analogWrite(motorPin1, 0);
-  delay(100);
-  //analogWrite(motorPin2, speed);
-  //delay(250);
-  //analogWrite(motorPin2, 0);
-  //analogWrite(motorPin3, speed);
-  //delay(250);
-  //analogWrite(motorPin3, 0);
-  //delay(100);
-  analogWrite(motorPin4, speed);
-  delay(120);
-  analogWrite(motorPin4, 0);
-  delay(100);
+  do
+  {
+    // analogWrite(motorPin1, speed);
+    // delay(120);
+    // analogWrite(motorPin1, 0);
+    // delay(100);
+    analogWrite(motorPin2, speed);
+    delay(250);
+    analogWrite(motorPin2, 0);
+    analogWrite(motorPin3, speed);
+    delay(250);
+    analogWrite(motorPin3, 0);
+    delay(100);
+    // analogWrite(motorPin4, speed);
+    // delay(120);
+    // analogWrite(motorPin4, 0);
+    // delay(100);
 
-  
-  valueFrente = analogRead(sensorFrente);
-  valueTras = analogRead(sensorTras);
-  valueArd = analogRead(sensorArd);
-  valueAmp = analogRead(sensorAmp);
-  Serial.print("Frente: ");
-  Serial.print(valueFrente);
-  Serial.print("Trás: ");
-  Serial.print(valueTras);
-  Serial.print("\n");
-  } while (valueTras > 650 && valueFrente > 850 );  //&& valueArd > 750 && valueAmp > 
-
+    valueFrente = analogRead(sensorFrente);
+    valueTras = analogRead(sensorTras);
+    Serial.print("Frente: ");
+    Serial.print(valueFrente);
+    Serial.print("Trás: ");
+    Serial.print(valueTras);
+    Serial.print("\n");
+  } while (valueFrente > maxDistFrente && valueTras > maxDistTras);
 }
-void loop(){
+void loop()
+{
 
- State= analogRead(PhotoIn);
- //Serial.print(State);
- //Serial.print("\n");
- delay(1000); 
-  
+  State = analogRead(PhotoIn);
+  // Serial.print(State);
+  // Serial.print("\n");
+  delay(1000);
 }
